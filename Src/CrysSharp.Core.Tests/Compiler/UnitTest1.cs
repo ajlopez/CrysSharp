cfafe913b7104c567a4be498028699a5bd4fe87d
ajlopez <webmaster@ajlopez.com>
2016-02-05 11:12:57 -0300

    Lexer NextToken is null

---
+++ Src/CrysSharp.Core.Tests/Compiler/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace CrysSharp.Core.Tests.Compiler
+{
+    using System;
+    using CrysSharp.Core.Compiler;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LexerTests
+    {
+        [TestMethod]
+        public void NextTokenIsNull()
+        {
+            Lexer lexer = new Lexer();
+
+            Assert.IsNull(lexer.NextToken());
+        }
+    }
+}
