3023860fb4c41be5ebb3ad780bad169399c1ce1e
ajlopez <webmaster@ajlopez.com>
2016-06-05 13:05:19 -0300

    NameExpression first creation test

---
+++ Src/CrysSharp.Core.Tests/Expressions/NameExpressionTests.cs
@@ -0,0 +1,18 @@
+﻿namespace CrysSharp.Core.Tests.Expressions
+{
+    using System;
+    using CrysSharp.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NameExpressionTests
+    {
+        [TestMethod]
+        public void CreateWithName()
+        {
+            NameExpression expr = new NameExpression("foo");
+
+            Assert.AreEqual("foo", expr.Name);
+        }
+    }
+}
---
+++ Src/CrysSharp.Core/Expressions/NameExpression.cs
@@ -0,0 +1,19 @@
+﻿namespace CrysSharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NameExpression
+    {
+        private string name;
+
+        public NameExpression(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+    }
+}
