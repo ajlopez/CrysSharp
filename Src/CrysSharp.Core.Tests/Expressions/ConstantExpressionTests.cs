6a321555f1714d3b56f27985890864922fba7c8d
ajlopez <webmaster@ajlopez.com>
2016-06-08 07:42:40 -0300

    ConstantExpression first creation test

---
+++ Src/CrysSharp.Core.Tests/Expressions/ConstantExpressionTests.cs
@@ -0,0 +1,21 @@
+﻿namespace CrysSharp.Core.Tests.Expressions
+{
+    using System;
+    using System.IO;
+    using CrysSharp.Core.Compiler;
+    using CrysSharp.Core.Exceptions;
+    using CrysSharp.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public class ConstantExpressionTests
+    {
+        [TestMethod]
+        public void CreateInstanceWithValue()
+        {
+            var expr = new ConstantExpression(42);
+
+            Assert.IsNotNull(expr.Value);
+            Assert.AreEqual(42, expr.Value);
+        }
+    }
+}
