3f7877c535d473bc4ed0c148383b29e0ae28a4d5
ajlopez <webmaster@ajlopez.com>
2016-06-10 06:08:32 -0300

    Parser parse empty string

---
+++ Src/CrysSharp.Core.Tests/Compiler/ParserTests.cs
@@ -0,0 +1,21 @@
+﻿namespace CrysSharp.Core.Tests.Compiler
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using CrysSharp.Core.Compiler;
+
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void ParseEmptyString()
+        {
+            Parser parser = new Parser(string.Empty);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
+    }
+}
---
+++ Src/CrysSharp.Core/Compiler/Parser.cs
@@ -0,0 +1,20 @@
+﻿namespace CrysSharp.Core.Compiler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using CrysSharp.Core.Expressions;
+
+    public class Parser
+    {
+        public Parser(string text)
+        {
+        }
+
+        public NameExpression ParseExpression()
+        {
+            return null;
+        }
+    }
+}
