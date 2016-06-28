0dd156f16d62bca61cba491e94a7aafb626e170a
ajlopez <webmaster@ajlopez.com>
2016-06-28 09:37:46 -0300

    Parse Logical Or Expression

--- Src/CrysSharp.Core.Tests/Compiler/ParserTests.cs
+++ Src/CrysSharp.Core.Tests/Compiler/ParserTests.cs
@@ -306,5 +306,31 @@
 
             Assert.IsNull(parser.ParseExpression());
         }
+
+        [TestMethod]
+        public void ParseLogicalOrBooleans()
+        {
+            Parser parser = new Parser("true||false");
+
+            var result = parser.ParseExpression();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(LogicalOrExpression));
+
+            var subexpr = (LogicalOrExpression)result;
+
+            var lexpr = subexpr.LeftExpression;
+            var rexpr = subexpr.RightExpression;
+
+            Assert.IsNotNull(lexpr);
+            Assert.IsInstanceOfType(lexpr, typeof(ConstantExpression));
+            Assert.AreEqual(true, ((ConstantExpression)lexpr).Value);
+
+            Assert.IsNotNull(rexpr);
+            Assert.IsInstanceOfType(rexpr, typeof(ConstantExpression));
+            Assert.AreEqual(false, ((ConstantExpression)rexpr).Value);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
     }
 }
--- Src/CrysSharp.Core/Compiler/Parser.cs
+++ Src/CrysSharp.Core/Compiler/Parser.cs
@@ -39,6 +39,8 @@
                     expr = new EqualsExpression(expr, this.ParseExpression());
                 else if (token.Value == "&&")
                     expr = new LogicalAndExpression(expr, this.ParseExpression());
+                else if (token.Value == "||")
+                    expr = new LogicalOrExpression(expr, this.ParseExpression());
             }
             else
                 this.lexer.PushToken(token);
