95a92d960a312973dc19c59e254199ef3530584d
ajlopez <webmaster@ajlopez.com>
2016-07-18 08:07:14 -0300

    Parse Less Expression

--- Src/CrysSharp.Core/Compiler/Parser.cs
+++ Src/CrysSharp.Core/Compiler/Parser.cs
@@ -51,6 +51,8 @@
                     expr = new EqualsExpression(expr, this.ParseExpression());
                 else if (token.Value == "!=")
                     expr = new NotEqualsExpression(expr, this.ParseExpression());
+                else if (token.Value == "<")
+                    expr = new LessExpression(expr, this.ParseExpression());
                 else if (token.Value == "&&")
                     expr = new LogicalAndExpression(expr, this.ParseExpression());
                 else if (token.Value == "||")
