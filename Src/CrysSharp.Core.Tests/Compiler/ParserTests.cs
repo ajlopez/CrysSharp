3436c123b420b4f528ae76156db8ea19b37ce220
ajlopez <webmaster@ajlopez.com>
2016-08-01 08:37:16 -0300

    Parse add multiply three integers, using precedence, WIP

--- Src/CrysSharp.Core.Tests/Compiler/ParserTests.cs
+++ Src/CrysSharp.Core.Tests/Compiler/ParserTests.cs
@@ -322,6 +322,42 @@
         }
 
         [TestMethod]
+        public void ParseAddMultiplyThreeIntegers()
+        {
+            Parser parser = new Parser("1+2*3");
+
+            var result = parser.ParseExpression();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(AddExpression));
+
+            var addexpr = (AddExpression)result;
+
+            var lexpr = addexpr.LeftExpression;
+            var rexpr = addexpr.RightExpression;
+
+            Assert.IsNotNull(lexpr);
+            Assert.IsInstanceOfType(lexpr, typeof(ConstantExpression));
+            Assert.AreEqual(1, ((ConstantExpression)rexpr).Value);
+
+            Assert.IsNotNull(rexpr);
+            Assert.IsInstanceOfType(rexpr, typeof(MultiplyExpression));
+
+            var multexpr = (MultiplyExpression)lexpr;
+
+            lexpr = multexpr.LeftExpression;
+            rexpr = multexpr.RightExpression;
+
+            Assert.AreEqual(2, ((ConstantExpression)lexpr).Value);
+
+            Assert.IsNotNull(rexpr);
+            Assert.IsInstanceOfType(rexpr, typeof(ConstantExpression));
+            Assert.AreEqual(3, ((ConstantExpression)rexpr).Value);
+
+            Assert.IsNull(parser.ParseExpression());
+        }
+
+        [TestMethod]
         public void ParseMultiplyAddThreeIntegersUsingParenthesis()
         {
             Parser parser = new Parser("1*(2+3)");
