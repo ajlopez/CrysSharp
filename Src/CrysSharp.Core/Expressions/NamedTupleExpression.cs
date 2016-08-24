385b828677187feb5aa53c2e2979dece32965ac2
ajlopez <webmaster@ajlopez.com>
2016-08-24 11:35:05 -0300

    NamedTupleExpression WIP

---
+++ Src/CrysSharp.Core/Expressions/NamedTupleExpression.cs
@@ -0,0 +1,23 @@
+﻿namespace CrysSharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NamedTupleExpression : IExpression
+    {
+        private List<String> names;
+        private List<IExpression> expressions;
+
+        public NamedTupleExpression(List<String> names, List<IExpression> expressions)
+        {
+            this.names = names;
+            this.expressions = expressions;
+        }
+
+        public List<String> Names { get { return this.names; } }
+
+        public List<IExpression> Expressions { get { return this.expressions; } }
+    }
+}
