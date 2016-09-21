7ac7b2c1e407f0cdd73d49bf8f1decc1b7edca86
ajlopez <webmaster@ajlopez.com>
2016-09-21 14:22:13 -0300

    Variable assignment expression

---
+++ Src/CrysSharp.Core.Tests/Expressions/VariableAssignmentExpressionTests.cs
@@ -0,0 +1,20 @@
+﻿namespace CrysSharp.Core.Tests.Expressions
+{
+    using System;
+    using CrysSharp.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class VariableAssignmentExpressionTests
+    {
+        [TestMethod]
+        public void CreateWithNameAndExpression()
+        {
+            ConstantExpression cexpr = new ConstantExpression(1);
+            VariableAssignmentExpression expr = new VariableAssignmentExpression("foo", cexpr);
+
+            Assert.AreEqual("foo", expr.Name);
+            Assert.AreSame(cexpr, expr.Expression);
+        }
+    }
+}
---
+++ Src/CrysSharp.Core/Expressions/VariableAssignmentExpression.cs
@@ -0,0 +1,23 @@
+﻿namespace CrysSharp.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class VariableAssignmentExpression : IExpression
+    {
+        private string name;
+        private IExpression expression;
+
+        public VariableAssignmentExpression(string name, IExpression expression)
+        {
+            this.name = name;
+            this.expression = expression;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IExpression Expression { get { return this.expression; } }
+    }
+}
