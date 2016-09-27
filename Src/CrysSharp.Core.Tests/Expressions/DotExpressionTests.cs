04f4ff4ce428fd7f53c9a757607695409ea117ba
ajlopez <webmaster@ajlopez.com>
2016-09-27 15:23:29 -0300

    Dot Expression first simple test

---
+++ Src/CrysSharp.Core.Tests/Expressions/DotExpressionTests.cs
@@ -0,0 +1,21 @@
+﻿namespace CrysSharp.Core.Tests.Expressions
+{
+    using System;
+    using CrysSharp.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DotExpressionTests
+    {
+        [TestMethod]
+        public void CreateWithVariableAndName()
+        {
+            DotExpression expr = new DotExpression(new VariableExpression("bar"), "foo");
+
+            Assert.IsNotNull(expr.Expression);
+            Assert.IsInstanceOfType(expr.Expression, typeof(VariableExpression));
+            Assert.AreEqual("bar", ((VariableExpression)expr.Expression).Name);
+            Assert.AreEqual("foo", expr.Name);
+        }
+    }
+}
